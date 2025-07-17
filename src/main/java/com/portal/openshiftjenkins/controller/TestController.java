99b9e1317bb83b9409188aa1dc40a4768658bb6e
multilynxmbilal <m.bilal@multilynx.pk>
2025-07-17 12:30:10 +0500

    Update TestController.java

--- src/main/java/com/portal/openshiftjenkins/controller/TestController.java
+++ src/main/java/com/portal/openshiftjenkins/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
 	@GetMapping("/hello")
 	public String ping() {
-		return "Openshift Pipe Line Testing";
+		return "Openshift Pipe Line Testing for the bilal";
 	}
 }
